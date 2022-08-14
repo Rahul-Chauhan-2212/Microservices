9e8dc2c23a53453981b122ab26f1029fef92d53a
RAHUL CHAUHAN <rchauhan9102@gmail.com>
2022-08-14 23:31:00 +0530

    Communication between user and contacts Microservices

--- user-service/src/main/java/com/users/userservice/controller/UserController.java
+++ user-service/src/main/java/com/users/userservice/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.users.userservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.users.userservice.entity.Contact;
 import com.users.userservice.entity.User;
 import com.users.userservice.service.UserService;
 
@@ -15,11 +20,20 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${contact.service.url}")
+	private String contactServiceUrl;
 
 	@GetMapping("/{id}")
 	public User getUser(@PathVariable("id") Long userId) {
 
-		return userService.getUser(userId);
+		User user = userService.getUser(userId);
+		List<Contact> listOfContacts = restTemplate.getForObject(contactServiceUrl+"/"+user.getUserId(), List.class);
+		user.setContacts(listOfContacts);
+		return user;
 	}
 
 }
